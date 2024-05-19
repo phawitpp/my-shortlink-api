470f4fb823b473fef0ef09919479233c7e69cba6
Phawit Monchaising <91412948+phawitpp@users.noreply.github.com>
2024-05-19 09:54:56 +0700

    Update cors

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ func main() {
 	app.Use(cors.New(
 		cors.Config{
 			AllowOrigins: "http://localhost:3000, https://s.phawit.tech",
-			AllowHeaders: "Origin, Content-Type, Accept",
+    			AllowHeaders:  "Origin, Content-Type, Accept, Authorization",
+    			AllowMethods:  "GET, POST, PUT, DELETE, OPTIONS",
+    			AllowCredentials: true,
 		},
 	))
 	database.ConnectDB()
