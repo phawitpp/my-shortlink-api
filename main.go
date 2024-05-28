3810bae822da20349e9be3d5166baff032dcee96
Phawit Monchaising <91412948+phawitpp@users.noreply.github.com>
2024-05-28 14:22:35 +0700

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := fiber.New()
 	app.Use(cors.New(
 		cors.Config{
-			AllowOrigins: "http://localhost:3000, https://s.phawit.tech, s.phawit.tech",
+			AllowOrigins: "http://localhost:3000, https://s.phawit.tech",
     			AllowHeaders:  "Origin, Content-Type, Accept, Authorization",
     			AllowMethods:  "GET, POST, PUT, DELETE, OPTIONS",
     			AllowCredentials: true,
